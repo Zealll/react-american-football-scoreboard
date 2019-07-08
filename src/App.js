f09541c05f91913dfa677d5dd1bd620a630e9f3a
Zealll <eriznis@gmail.com>
2019-07-08 16:06:08 -0400

    added useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //TODO: STEP 1 - Import the useState hook.
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
@@ -7,7 +7,24 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeScore, setHomeScore] = useState(32)
   const [awayScore, setAwayScore] = useState(32)
+  const [time, setTime] = useState(10)
 
+
+  
+  
+  const timer = () => {
+     time > 0 ? setTime(time - 1) : setTime(10)
+  }
+  
+  useEffect(() => {
+    const interval = setInterval(timer, 1000)
+    return () => clearInterval(interval)
+  })
+
+  
+  
+
+  // Home Team Methods
   const homeTouchdown = () => {
     setHomeScore(homeScore + 7)
   }
@@ -15,6 +32,8 @@ function App() {
     setHomeScore(homeScore + 3)
   }
 
+
+  // Away Team Methods
   const awayTouchdown = () => {
     setAwayScore(awayScore + 7)
   }
@@ -32,7 +51,7 @@ function App() {
 
             <div className="home__score">{homeScore}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="timer">{time}</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
             <div className="away__score">{awayScore}</div>
